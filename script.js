fcd0248cf66f789a20a0beb07473a0ce19abb091
q50343 <q503433@yahoo.com.tw>
2020-05-27 14:01:03 +0800

    fix

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // 環境變數
 let updateFPS = 30
+let showMouse = true
 let time = 0
 let bgColor = '#000'
+
+// 控制
+let controls = {
+    value: 0
+}
+let gui = new dat.GUI()
+gui.add(controls,'value',-2,2).step(0.01).onChange(value => {
+
+})
+
 // -----------
 // Vec2
 class Vec2{
@@ -46,66 +57,6 @@ class Vec2{
     }
 }
 
-let degToPi = Math.PI / 180
-class Circle{
-    constructor(args){
-        let def = {
-            p: new Vec2(),
-            r: 100,
-            color: '#fff',
-            lineTo: (obj,i) => {
-                return true
-            },
-            getWidth: (obj,i) => {
-                return 1
-            },
-            anglePan: (obj,i) => {
-                return 0
-            },
-            vertical: false,
-            getVerticalWidth: (obj,i) => {
-                return 2
-            },
-            ramp: 0
-        }
-        Object.assign(def,args)
-        Object.assign(this,def)
-    }
-    draw(){
-        ctx.beginPath()
-        for(let i = 1;i <= 360;i++) {
-            let angle1 = i + this.anglePan()
-            let angle2 = i - 1 + this.anglePan()
-            let use_r = this.r + this.ramp * Math.sin(i/10)
-            let use_r2 = this.r + this.ramp * Math.sin((i-1)/10)
-
-            let x1 = use_r * Math.cos(angle1 * degToPi)
-            let y1 = use_r * Math.sin(angle1 * degToPi)
-            let x2 = use_r2 * Math.cos(angle2 * degToPi)
-            let y2 = use_r2 * Math.sin(angle2 * degToPi)
-            
-            if (this.lineTo(this,i)) {
-                ctx.beginPath()
-                ctx.line(new Vec2(x1,y1),new Vec2(x2,y2))
-                ctx.strokeStyle = this.color
-                ctx.lineWidth = this.getWidth(this,i)
-                ctx.stroke()
-            }
-            if (this.vertical) {
-                let l = this.getVerticalWidth(this,i)
-                let x3 = (use_r + l) * Math.cos(angle1 * degToPi)
-                let y3 = (use_r + l) * Math.sin(angle1 * degToPi)
-
-                ctx.beginPath()
-                ctx.line(new Vec2(x1,y1),new Vec2(x3,y3))
-                ctx.strokeStyle = this.color
-                ctx.lineWidth = this.getWidth(this,i)
-                ctx.stroke()
-            }
-        }
-    }
-}
-
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
 ctx.circle = function(v,r,color) {
@@ -124,64 +75,8 @@ function initCanvas() {
     ww = canvas.width = window.innerWidth
     wh = canvas.height = window.innerHeight
 }
-let cirs = []
 
 function init() {
-    cirs.push(new Circle({
-        r: 150,
-        color: 'rgba(255,255,255,0.4)'
-    }))
-    cirs.push(new Circle({
-        r: 220,
-        lineTo:(obj,i) => {
-            return i % 2 === 0
-        },
-        color: 'rgba(255,255,255,1)'
-    }))
-    cirs.push(new Circle({
-        r: 80,
-        lineTo:(obj,i) => {
-            return i % 180 > 30
-        }
-    }))
-    cirs.push(new Circle({
-        r: 320,
-        ramp: 15,
-        color: 'rgba(255,255,255,1)'
-    }))
-    cirs.push(new Circle({
-        r: 190,
-        ramp: 0,
-        getWidth:(obj,i) => {
-            return i % 150 < 30 ? 5 : 1
-        }
-    }))
-    cirs.push(new Circle({
-        r: 300,
-        ramp: 0,
-        vertical: true,
-        lineTo:(obj,i) => {
-            return false
-        },
-        getVerticalWidth:(obj,i) => {
-            return i % 10 === 0 ? 10 : i % 5 === 0 ? 5 : 2
-        },
-        anglePan:(obj,i) => {
-            return 40 * Math.sin(time/200)
-        }
-    }))
-    cirs.push(new Circle({
-        r: 280,
-        lineTo:(obj,i) => {
-            return i % 50 === 0
-        },
-        getWidth:(obj,i) => {
-            return 10
-        },
-        anglePan:(obj,i) => {
-            return (-time/20) % 5
-        }
-    }))
 
 }
 function update() {
@@ -192,65 +87,6 @@ function draw() {
     ctx.fillStyle = bgColor
     ctx.fillRect(0,0,ww,wh)
     // --------
-    ctx.save()
-        ctx.translate(ww/2,wh/2)
-        cirs.forEach(c => {
-            ctx.save()
-                let pan = mousePos.sub(new Vec2(ww/2,wh/2)).mul(3/c.r)
-                ctx.translate(pan.x,pan.y)
-                c.draw()
-            ctx.restore()
-        })
-
-        ctx.fillStyle = '#fff'
-        ctx.fillRect(0,-20,120,20)
-        ctx.fillStyle = '#000'
-        ctx.fillText(Date.now(),5,-5)
-        // 指針
-        let h = new Date().getHours()
-        let m = new Date().getMinutes()
-        let s = new Date().getSeconds()
-
-        let angleH = degToPi * 30 * h - Math.PI / 2
-        let angleM = degToPi * 6 * m - Math.PI / 2
-        let angleS = degToPi * 6 * s - Math.PI / 2
-
-        ctx.line(new Vec2(0,0),new Vec2(50*Math.cos(angleH),50*Math.sin(angleH)))
-        ctx.lineWidth = 5
-        ctx.strokeStyle = 'red'
-        ctx.stroke()
-
-        ctx.beginPath()
-        ctx.line(new Vec2(0,0),new Vec2(100*Math.cos(angleM),100*Math.sin(angleM)))
-        ctx.lineWidth = 2
-        ctx.strokeStyle = '#fff'
-        ctx.stroke()
-
-        ctx.beginPath()
-        ctx.line(new Vec2(0,0),new Vec2(150*Math.cos(angleS),150*Math.sin(angleS)))
-        ctx.lineWidth = 1
-        ctx.strokeStyle = '#fff'
-        ctx.stroke()
-    ctx.restore()
-    // 十字
-    let crosses = [
-        new Vec2(50,50),
-        new Vec2(ww-50,50),
-        new Vec2(50,wh-50),
-        new Vec2(ww-50,wh-50)
-    ]
-
-    crosses.forEach(c => {
-        ctx.beginPath()
-        ctx.save()
-            ctx.translate(c.x,c.y)
-            ctx.line(new Vec2(-10,0),new Vec2(10,0))
-            ctx.line(new Vec2(0,-10),new Vec2(0,10))
-            ctx.lineWidth = 2
-            ctx.strokeStyle = '#fff'
-            ctx.stroke()
-        ctx.restore()
-    })
 
     // ----------
     ctx.circle(mousePos,3,'red')
